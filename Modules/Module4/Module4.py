4d65734961401960760eafa5b7ac0ef9e77957cd
MaximilianoDavila <25-070@sanignacio.pr>
2021-09-08 09:26:23 -0400

    Module4.py

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,9 @@
+meal = "pizza"
+print("what did you have for breakfast")
+print(meal)
+print("what was your lunch?")
+meal = "sushi" 
+print(meal)
+print("Have you had dinner")
+meal = "Yes, I had pasta and chicken"
+print(meal)
