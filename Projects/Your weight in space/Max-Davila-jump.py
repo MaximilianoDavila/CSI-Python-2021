e12f3a6b9a150169ab4d49b053e1f32420502596
MaximilianoDavila <25-070@sanignacio.pr>
2021-10-05 08:01:05 -0400

    Jump

--- Projects/Your weight in space/Max-Davila-jump.py
+++ Projects/Your weight in space/Max-Davila-jump.py
@@ -1,14 +1,15 @@
-planets = {"Mercury", "Venus", "Earth", "Mars","Jupiter", "Saturn", "Uranus", "Neptune"}
+planet = ["Mercury", "Venus", "Earth", "Mars","Jupiter", "Saturn", "Uranus", "Neptune"]
 rel_gravity = [2.65, 1.11, 2.64, 0.40, 0.94, 1.19, 0.88]
 
 print("small step for space and a big step on other planets")
 myjump = float(input("What is your jump's lenght on Earth(meter)? "))
 
-myplanets = input(f"Select a planet from the list: {planets}\n")
-def Calculatejump(planet, meter):
+myplanets = input(f"Select a planet from the list: {planet}")
+def Calculatejump(planets, meter):
     print(f"your jump in Earth is: {meter}")
 
-    planet_index = planets.index(planet)
+    planet_index = planet.index(planets)
 
-    print(f"Your weight in {planets[planet_index]} is {(myjump * rel_gravity[planet_index])} meters.")
+    print(f"Your weight in {planet[planet_index]} is {(myjump * rel_gravity[planet_index])} meters.")
+
 Calculatejump(myplanets, myjump)
