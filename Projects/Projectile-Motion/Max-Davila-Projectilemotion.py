163f6865f6ce05bfb62cbfb551991c9e81bd2573
MaximilianoDavila <25-070@sanignacio.pr>
2021-11-15 08:32:18 -0400

    Experimental

--- Projects/Projectile-Motion/Max-Davila-Projectilemotion.py
+++ Projects/Projectile-Motion/Max-Davila-Projectilemotion.py
@@ -1,3 +1,4 @@
+import math
 # gun = 'ak-101'
 # caliber = '5.56x45 mm'
 # ammunition = 'Warmageddon'
@@ -12,13 +13,41 @@
 
 
 
-def projectilefuction(gun:str, caliber:str, ammunition:str, Velocity_ms:int, building:str, buildingheigt:int, gravity_Ms:int):
-    time_s = math.sqrt((2 * buildingheight) / gravity_Ms)
-    distance_m = (Velocity_ms * time_s)
+def projectilefuction(experimentaldata: Experimentaldata):
+    time_s = math.sqrt((2 * experimentaldata.buildingheight) / experimentaldata.gravity_Ms)
+    distance_m = (experimentaldata.velocity_ms * time_s)
 
-    print(f"The gun selected for this experiment is {gun}. the caliber of {gun} is {caliber}, and the ammunition of the {gun} is {ammunition}. The spped of the a the ammunition is {Velocity_ms}.")
-    print(f'the building that we are going to use is the {building} in sanjuan, and the height of the {building} is {buildingheight}.')
+    print(f"The gun selected for this experiment is {experimentaldata.gun}. the caliber of {experimentaldata.gun} is {experimentaldata.caliber}, and the ammunition of the {experimentaldata.gun} is {experimentaldata.ammunition}. The spped of the a the ammunition is {Velocity_ms}.")
+    print(f'the building that we are going to use is the {experimentaldata.building} in sanjuan, and the height of the {experimentaldata.building} is {experimentaldata.buildingheight}.')
     print('This day we were lucky that there is no air resistance.')
 
-projectilefuction("ak-101", "5.56x46 mm", "warmageddon", 910, "Minillas South Tower", "243.69", 9.81)
+# Convert your script parameters into a single Json object
+# experimentaldata = (
+
+# "gun" : "Ak 101",
+# "caliber" : "5.56x45 mm",
+# "ammunition" : "Warmageddon",
+# 'building' : "Minillas South Tower",
+# "Velocity_ms" : "910",
+# "buildingheight" : "243.69"
+# "gravity_Ms" : "9.81"
+
+# )
+
+# experimentaldata = ExperimentalData("ak 101", "5.56x45 mm", "Warmageddon", 910, "Minilas South Tower", 243.69, 9.81)
+
+myDataset = [
+ExperimentalData("ak-101", "5.56x46 mm", "warmageddon","MB556A1", 910, "Minillas South Tower", 243.69, 9.81),
+ExperimentalData("ak-101", "5.56x46 mm", "warmageddon","MBSS", 910, "Minillas South Tower", 243.69, 9.81),
+ExperimentalData("ak-101", "5.56x46 mm", "warmageddon","MB", 910, "Minillas South Tower", 243.69, 9.81),
+ExperimentalData("ak-101", "5.56x46 mm", "warmageddon","MBSS", 910, "Minillas South Tower", 243.69, 9.81),
+
+]
+for data in myDataset:
+    print("\n----------------------------------------------\n")
+    projectilefunction(data)
+
+# projectilefuction("ak-101", "5.56x46 mm", "warmageddon", 910, "Minillas South Tower", "243.69", 9.81)
+
+
 
