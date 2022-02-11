321c0322dd6c2e76d5d26ea4552eda03e3f1ec16
CSI-Maximiliano-Davila <25-070@sanignacio.pr>
2022-02-11 09:23:45 -0400

    Update Maximiliano-Davila.Snakeman.py

--- Projects/Projectile-Motion/Maximiliano-Davila.Snakeman.py
+++ Projects/Projectile-Motion/Maximiliano-Davila.Snakeman.py
@@ -1,22 +1,50 @@
+
 import pygame
 pygame.init()
 
 
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+yellow = (255, 255, 102) #score
+black = (0, 0, 0) #food
+red = (213, 50, 80) #background color
+green = (0, 255, 0) #snake
+blue = (50, 153, 213) #game over
 
-dis = pygame.display.set_mode((400, 300))
-pygame.display.update()
+dis = pygame.display.set_mode((900, 800))
+
 pygame.display.set_caption('Snake game by JD')
 game_over=False
+
+x1 = 400
+y1 = 400
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
 while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
-        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                 x1_change = 10
+                 y1_change = 0
+            elif event.key == pygame.K_UP:
+                 x1_change = 0
+                 y1_change = -10
+            elif event.key == pygame.K_DOWN:
+                 x1_change = 0
+                 y1_change = 10
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(yellow)
+    pygame.draw.rect(dis, blue, [x1, y1, 10, 10]) # crea el snake y lo ubica en la pantalla
+    pygame.display.update()
 
+
+    clock.tick(30)
 pygame.quit()
 quit()
